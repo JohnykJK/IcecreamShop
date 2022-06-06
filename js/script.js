baab19a45c0c0d814403dba1de558e1a2059efb0
JohnykJK <johnykdsr@gmail.com>
2022-06-06 18:05:59 +0200

    body menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const iconMenu = document.querySelector('.burger__icon')
+const menuBody = document.querySelector('.nav-menu')
+const bodyLock = document.querySelector('body')
 if (iconMenu) {
     iconMenu.addEventListener('click', function (e){
         iconMenu.classList.toggle('open');
+        menuBody.classList.toggle('open');
+        bodyLock.classList.toggle('lock')
     });
 }
